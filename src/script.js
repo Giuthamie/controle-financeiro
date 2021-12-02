219a2e1070d1c6630886438a7ed7eb337341d39e
giuthamie <giumie@hotmail.com>
2021-12-02 11:34:15 -0300

    implementaçoes de transaçoes, manipulaçao dom para add lis com valores das transaçoes e inicio da funçao que add as transaçoes ao dom'

---
+++ src/script.js
@@ -0,0 +1,36 @@
+const transactionsUl = document.querySelector('#transactions')
+
+/* Transações ficticias, contendo objetos com id, nome e valor da transação*/
+
+const dummyTransactions = [
+{ id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+{ id: 2, name: 'Salário', amount: 300 },
+{ id: 3, name: 'Torta de frango', amount: -10 },
+{ id: 4, name: 'Violão', amount: 150 },
+]
+
+/*manipulação no dom para add as transações nas li's*/
+
+const addTransactionIntoDom = transaction => {
+    /* Se a condição resultar em true, armazenará um string - se não uma string + */
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const li = document.createElement('li');
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator} </span><button class="delete-btn">x</button>
+    `
+    transactionsUl.append(li)
+}
+
+
+const updateBalanceValues = () => {
+    const transactionsAmount = dummyTransactions.map(transaction => transaction.amount)
+}
+/* Função que executa add as transações no dom*/
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDom)
+}
